43805d533e6ad33b1155b04070774b5edf7b4762
Renzo jutte <renzojutte@gmail.com>
2025-11-14 11:51:18 +0100

    style: added cleaner style to my quill editor with css

--- Dev/resources/js/app.js
+++ Dev/resources/js/app.js
@@ -9,19 +9,19 @@ Alpine.start();
 import './bootstrap';
 import { QuillEditor } from './quill-editor';
 
-// Initialize Quill editors when DOM is ready
+
 document.addEventListener('DOMContentLoaded', () => {
-    // For topics/create and topics/edit
+
     if (document.querySelector('#topic-editor')) {
         new QuillEditor('#topic-editor', '#topic-body-input');
     }
 
-    // For replies
+
     if (document.querySelector('#reply-editor')) {
         new QuillEditor('#reply-editor', '#reply-body-input');
     }
 
-    // For threads (if you want rich text there too)
+
     if (document.querySelector('#thread-editor')) {
         new QuillEditor('#thread-editor', '#thread-description-input');
     }
